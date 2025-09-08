dc192e2d9e745ca7213d76260d9839d43f820384
Bankole Ifeoluwa <ifeoluwa.bankole05@gmail.com>
2025-09-08 12:21:43 +0100

    First commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <p>Boy</p>
+      <a href="d" className='decoration-black'></a>
+    </div>
+  )
+}
+
+export default Header
